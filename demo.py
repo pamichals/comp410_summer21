20b78fac57f367e45f25ea90d212e43f32708597
pamichals <pamichals@aggies.ncat.edu>
2021-07-26 20:24:46 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -34,6 +34,20 @@ def asa_parse():
     fig = table.plot().get_figure()
     fig.savefig('plot.png')
 
+def team3_asa_parse():
+    # Parse the ASA logfile
+    parser = LogParse()
+    df = parser.parse_asa_logfile('dmz1_logs.txt')
+    dft3 = df[df['ID'].isin(['114006', '114009', '114014'])]
+    print(dft3.describe())
+
+    # Create a table by hour with the total count of messages seen
+    table = dft3.groupby([pd.Grouper(key='Date', freq='H')]).agg({'ID': 'count'})
+
+    # Create a simple plot and save as a png
+    fig = table.plot().get_figure()
+    fig.savefig('team3plot.png')
+
 
 if __name__ == "__main__":
     print(show_aggie_pride())
@@ -42,5 +56,7 @@ if __name__ == "__main__":
     parse_logs()
 
     # Uncomment when we start sprint-4
-    # print('\nCalling asa_parse()')
-    # asa_parse()
+    print('\nCalling asa_parse()')
+    asa_parse()
+    print('\nCalling team3_asa_parse')
+    team3_asa_parse()
